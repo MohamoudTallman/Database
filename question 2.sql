c0fbf6c963663719ee9eec31af387ad6cbd71f56
MohamoudTallman <38226201+MohamoudTallman@users.noreply.github.com>
2019-02-28 20:37:43 -0600

    Delete question 2.sql

--- question 2.sql
+++
@@ -1,8 +0,0 @@
-USE employees; 
-
-SELECT gender, MIN(salary), MAX(salary), AVG(salary),COUNT(e.emp_no) as 'Total Employees'
-FROM employees e, salaries s
-
-WHERE s.emp_no = e.emp_no
-
-GROUP BY gender
