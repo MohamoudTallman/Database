8115dcc552becfc3041c01a8bc46a0cac0a5f90f
MohamoudTallman <38226201+MohamoudTallman@users.noreply.github.com>
2019-02-28 20:37:33 -0600

    Delete question 3.sql

--- question 3.sql
+++
@@ -1,5 +0,0 @@
-SELECT COUNT(employees.emp_no) as 'Total Employees'
-FROM employees,dept_emp
-WHERE employees.emp_no=dept_emp.emp_no
-AND dept_emp.dept_no='d007'
-AND (employees.hire_date BETWEEN '1986-01-01' AND '1991-03-07');
